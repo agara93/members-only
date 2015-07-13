18f22d18e342ee26f225ecd30ee0a052edd7ac73
agara93 <adityagagat@gmail.com>
2015-07-13 07:02:23 +0000

    Finalized, no CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
- 
+  root 'posts#index'
   get   'signin'    => 'sessions#new'
   post  'signin'    => 'sessions#create'
+  
+  resources :posts,     only: [:new, :create, :index]
+  match '/signin' , to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
 end
